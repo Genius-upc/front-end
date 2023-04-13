62b5c8e13f2229c701922444605fa943b2709a6d
CDenisse97 <118797651+CDenisse97@users.noreply.github.com>
2023-04-13 18:36:32 -0500

    Vue components
    Signed-off-by: CDenisse97 <118797651+CDenisse97@users.noreply.github.com>

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/home',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/',
+      redirect: '/home'
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: () => import("../parking/pages/user-login.component.vue")
+    },
+    {
+      path: '/parkingList',
+      name: 'parkingList',
+      component: () => import("../parking/pages/park-list.component.vue")
+    }
+  ]
+})
+
+export default router
